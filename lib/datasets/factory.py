2b947934646dc893122af02790fe32af84883815
Alexander Hermann <s0099693@mail.zih.tu-dresden.de>
2017-11-01 08:19:43 +0100

    Adjust dataset to mms

--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -1,52 +1,89 @@
-# --------------------------------------------------------
-# Fast R-CNN
-# Copyright (c) 2015 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ross Girshick
-# --------------------------------------------------------
-
-"""Factory method for easily getting imdbs by name."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__sets = {}
 from datasets.pascal_voc import pascal_voc
-from datasets.coco import coco
 
+from .mmsvoc import mmsvoc
+from datasets.mmsvoc import mmsvoc
+#from datasets.mmsvoctest import mmsvoctest
+from datasets.mmsvocval import mmsvocval
 import numpy as np
 
-# Set up voc_<year>_<split> 
-for year in ['2007', '2012']:
-  for split in ['train', 'val', 'trainval', 'test']:
-    name = 'voc_{}_{}'.format(year, split)
-    __sets[name] = (lambda split=split, year=year: pascal_voc(split, year))
 
-for year in ['2007', '2012']:
-  for split in ['train', 'val', 'trainval', 'test']:
-    name = 'voc_{}_{}_diff'.format(year, split)
-    __sets[name] = (lambda split=split, year=year: pascal_voc(split, year, use_diff=True))
+
+
+__sets = {}
+
+def _selective_search_IJCV_top_k(split, year, top_k):
+    """Return an imdb that uses the top k proposals from the selective search
+    IJCV code.
+    """
+    imdb = pascal_voc(split, year)
+    imdb.roidb_handler = imdb.selective_search_IJCV_roidb
+    imdb.config['top_k'] = top_k
+    return imdb
+
+# Set up voc_<year>_<split> using selective search "fast" mode
+for year in ['2007', '2012', '0712']:
+    for split in ['train', 'val', 'trainval', 'test']:
+        name = 'voc_{}_{}'.format(year, split)
+        __sets[name] = (lambda split=split, year=year:
+                pascal_voc(split, year))
+
+
+# Set up kittivoc
+    for split in ['train', 'val', 'trainval', 'test']:
+        name = 'kittivoc_{}'.format(split)
+        print(name)
+        __sets[name] = (lambda split=split: kittivoc(split))
+# Set up mmsvoc
+for split in ['train', 'val', 'trainval', 'test']:
+    name = 'mmsvoc_{}'.format(split)
+    print(name)
+    __sets[name] = (lambda split=split: mmsvoc(split))
+
+#for split in ['train', 'val', 'trainval', 'test']:
+#    name = 'mmsvoctest_{}'.format(split)
+#    print(name)
+#    __sets[name] = (lambda split=split: mmsvoctest(split))
+
+for split in ['train', 'val', 'trainval', 'test']:
+    name = 'mmsvocval_{}'.format(split)
+    print(name)
+    __sets[name] = (lambda split=split: mmsvocval(split))
+# # KITTI dataset
+# for split in ['train', 'val', 'trainval', 'test']:
+#     name = 'kitti_{}'.format(split)
+#     print name
+#     __sets[name] = (lambda split=split: kitti(split))
 
 # Set up coco_2014_<split>
 for year in ['2014']:
-  for split in ['train', 'val', 'minival', 'valminusminival', 'trainval']:
-    name = 'coco_{}_{}'.format(year, split)
-    __sets[name] = (lambda split=split, year=year: coco(split, year))
+    for split in ['train', 'val', 'minival', 'valminusminival']:
+        name = 'coco_{}_{}'.format(year, split)
+        __sets[name] = (lambda split=split, year=year: coco(split, year))
 
 # Set up coco_2015_<split>
 for year in ['2015']:
-  for split in ['test', 'test-dev']:
-    name = 'coco_{}_{}'.format(year, split)
-    __sets[name] = (lambda split=split, year=year: coco(split, year))
+    for split in ['test', 'test-dev']:
+        name = 'coco_{}_{}'.format(year, split)
+        __sets[name] = (lambda split=split, year=year: coco(split, year))
+
+# NTHU dataset
+for split in ['71', '370']:
+    name = 'nthu_{}'.format(split)
+    print (name)
+    __sets[name] = (lambda split=split: nthu(split))
 
 
 def get_imdb(name):
-  """Get an imdb (image database) by name."""
-  if name not in __sets:
-    raise KeyError('Unknown dataset: {}'.format(name))
-  return __sets[name]()
-
+    """Get an imdb (image database) by name."""
+    if not __sets.has_key(name):
+        print (list_imdbs())
+        raise KeyError('Unknown datasets: {}'.format(name))
+    return __sets[name]()
 
 def list_imdbs():
-  """List all registered imdbs."""
-  return list(__sets.keys())
+    """List all registered imdbs."""
+    return __sets.keys()
